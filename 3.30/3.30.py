9d7e638ae97906ad7dd608df8c1ea1ad878c287a
zlnanytime <zlanytime@gmail.com>
2019-03-31 09:36:52 +0800

    ...

--- 3.30/3.30.py
+++ 3.30/3.30.py
@@ -1,6 +1,30 @@
-s = input('输入体重: ')
-weigh = int(s)
-if weigh<18.5:
-    print('过轻')
-elif weigh>=18.5 and weigh<25 :
-    print('b')
+# s = input('输入体重: ')
+# weigh = int(s)
+# if weigh<18.5:
+#     print('过轻')
+# elif weigh>=18.5 and weigh<25 :
+#     print('b')
+
+# names = ['Michel','Bob','Tracy']
+# for name in names:
+#     print(name)
+
+# s = 0;
+# for x in [1,2,3,4,5,6,7,8,9,10]:
+#     s = s + x
+# print(s)
+
+
+# print(list(range(5)))
+
+# s = 0
+# for x in range(101):
+#     s = s + x
+# print(s)
+
+# L = ['Bart','Lisa','Adam']
+# for name in L:
+#     print('Hello %s'%name)
+
+
+
